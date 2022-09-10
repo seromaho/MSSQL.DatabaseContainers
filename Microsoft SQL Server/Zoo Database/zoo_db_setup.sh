9e533cce09a7621dde91012228abf48923ae16a2
seromaho <seromaho@t-online.de>
2022-09-10 17:59:35 +0200

    improved shell scripts (again)

--- Microsoft SQL Server/Zoo Database/zoo_db_setup.sh
+++ Microsoft SQL Server/Zoo Database/zoo_db_setup.sh
@@ -3,41 +3,41 @@
 # Move the database's primary data file to MSSQL Server's default data directory location
 for i in {1..50};
 do
-    /usr/bin/mv -f /tmp/zoo_db.mdf /var/opt/mssql/data/zoo_db.mdf;
+    /usr/bin/mv -f /tmp/zoo_db.mdf /var/opt/mssql/data/zoo_db.mdf
     if [ ${?} -eq 0 ]
     then
-        /usr/bin/echo "SUCCESS: move zoo_db.mdf";
+        /usr/bin/echo "SUCCESS: move zoo_db.mdf"
         break
     else
-        /usr/bin/echo "TRY: move zoo_db.mdf";
-        sleep 1;
+        /usr/bin/echo "TRY: move zoo_db.mdf"
+        sleep 1
     fi
 done
 
 # Move the database's transaction log file to MSSQL Server's default log directory location
 for i in {1..50};
 do
-    /usr/bin/mv -f /tmp/zoo_db.ldf /var/opt/mssql/data/zoo_db.ldf;
+    /usr/bin/mv -f /tmp/zoo_db.ldf /var/opt/mssql/data/zoo_db.ldf
     if [ ${?} -eq 0 ]
     then
-        /usr/bin/echo "SUCCESS: move zoo_db.ldf";
+        /usr/bin/echo "SUCCESS: move zoo_db.ldf"
         break
     else
-        /usr/bin/echo "TRY: move zoo_db.ldf";
-        sleep 1;
+        /usr/bin/echo "TRY: move zoo_db.ldf"
+        sleep 1
     fi
 done
 
 # Execute the "zoo_db.sql" script that restores the database
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -U "SA" -P "P@ssw0rd" -l 30 -i "/tmp/zoo_db.sql";
+    /opt/mssql-tools/bin/sqlcmd -U "SA" -P "P@ssw0rd" -l 30 -i "/tmp/zoo_db.sql"
     if [ ${?} -eq 0 ]
     then
-        /usr/bin/echo "SUCCESS: create database zoo_db";
+        /usr/bin/echo "SUCCESS: create database zoo_db"
         break
     else
-        /usr/bin/echo "TRY: create database zoo_db";
-        sleep 1;
+        /usr/bin/echo "TRY: create database zoo_db"
+        sleep 1
     fi
 done
